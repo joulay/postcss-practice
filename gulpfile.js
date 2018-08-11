f92a970ac12165620374a16a931021139486e9a7
Julie K <julieskim160@gmail.com>
2018-08-11 01:00:19 -0700

    install cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
-
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 gulp.task('serve', ['css'], function(){
     gulp.watch("./css/*.css", ['css']);
@@ -9,6 +10,8 @@ gulp.task('serve', ['css'], function(){
 gulp.task('css', function() {
     var plugin = [
         //PostCSS Plugins Here
+        autoprefixer(),
+        cssnano()
     ];
     return gulp.src('./css/*.css')
         .pipe(postcss(plugin))

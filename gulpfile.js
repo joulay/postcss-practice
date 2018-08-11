53e57605f86c6007126ea416920d8dec874a7549
Julie K <julieskim160@gmail.com>
2018-08-11 00:50:24 -0700

    require files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 
-gulp.task('')
+
+gulp.task('serve', ['css'], function(){
+    gulp.watch("./css/*.css", ['css']);
+});
+
+gulp.task('css', function() {
+    var plugin = [
+        //PostCSS Plugins Here
+    ];
+    return gulp.src('./css/*.css')
+        .pipe(postcss(plugin))
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('default', ['serve']);
